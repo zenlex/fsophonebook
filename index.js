d9fab72418d098e4b89321a7bfde95a473bdb376
zenlex <erichrkeil@gmail.com>
2022-01-23 12:35:01 -0600

    switch to express

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
 const http = require('http')
+const express = require('express');
+
+const app = express();
 
 const persons = [
-  { 
+  {
     "id": 1,
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
-  { 
+  {
     "id": 2,
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": 3,
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": 4,
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ];
-
-const app = http.createServer((req, res) => {
-    switch(req.url) {
-      case '/api/persons':
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify(persons))
-        break;
-      case '/info':
-        const msg = `<p>Phonebook has info for ${persons.length} people</p>
+// GET all persons
+app.route('/api/persons').get((req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.json(persons)
+});
+// GET phonebook metadata
+app.route('/info').get((req, res) => {
+  const msg = `<p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date().toString()}</p>`
-        res.end(msg)
-    }
-})
+  res.send(msg)
+});
+// GET single person
+app.route('/api/persons/:id').get((req, res) => {
+  const id = Number(req.params.id);
+  console.log({ id });
+  person = persons.find(person => person.id === id);
+  res.json(person)
+});
+
+// 404 default
+app.use((req, res, next) => {
+  res.status = 404
+  res.send('page not found');
+});
 
 
 const PORT = 3001;
