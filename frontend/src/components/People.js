0feade824727b5661b01a39d7996d2283016754f
zenlex <erichrkeil@gmail.com>
2022-01-25 17:29:12 -0600

    copy front end from part 2

---
+++ frontend/src/components/People.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const PersonRow = ({ person, deletePerson }) => (
+  <li key={person.name}>
+    {person.name} - {person.number}
+    <button onClick={() => deletePerson(person.id)}>delete</button>
+  </li>
+)
+
+const People = ({ persons, deletePerson }) => (
+  <ul>
+    {persons.map(person => <PersonRow key={person.name} person={person} deletePerson={deletePerson}/>)}
+  </ul>
+)
+
+export default People;
